2e66a1a1fca9dca055b68bc56407ea6d3ffd6e2b
Stella Dunn <stellacdunn@gmail.com>
2024-03-08 18:47:36 -0700

    Theming

--- src/dashboard/settings.jsx
+++ src/dashboard/settings.jsx
@@ -1,12 +1,21 @@
+import { Container, Typography } from "@mui/material";
 import { useAuth } from "../hooks/useAuth";
 
 export function SettingsPage() {
 	const { isLoggedIn } = useAuth();
 
 	return (
-		<div>
-			<h1>This is the Settings page</h1>
-			<p>Logged in = {isLoggedIn().toString()}</p>
-		</div>
+		<Container
+			sx={{
+				bgcolor: 'primary.main',
+				paddingBottom: 2,
+				borderBottomLeftRadius: 5,
+				borderBottomRightRadius: 5
+			}}
+		>
+			<Container sx={{ bgcolor: 'white', borderRadius: 2 }}>
+				<Typography variant='h3'>This is the Settings page</Typography>
+			</Container>
+		</Container>
 	);
 }
