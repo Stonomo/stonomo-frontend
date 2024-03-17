797c25ca8ffe6060440391edd6a5c754694b3c7c
Stella Dunn <stellacdunn@gmail.com>
2024-03-17 10:27:16 -0600

    Populated User page

--- src/dashboard/user.jsx
+++ src/dashboard/user.jsx
@@ -1,16 +1,66 @@
-import { Outlet } from "react-router";
-import { useAuth } from "../hooks/useAuth";
-import { Container } from "@mui/material";
+import { Container, Unstable_Grid2 as Grid, Stack, Typography, styled } from "@mui/material";
+import { useLoaderData } from "react-router";
+
+const Label = styled(Typography)({
+	textAlign: 'right',
+	height: '100%',
+	width: '100%',
+	padding: '10px'
+})
+
+const Content = styled(Typography)({
+	textAlign: 'left',
+	height: '100%',
+	width: '100%',
+	paddingTop: '15px'
+})
 
 export function UserPage() {
-	const { isLoggedIn } = useAuth();
+	const userData = useLoaderData()
+	const address = userData.facilityAddress
 
 	return (
-		<Container sx={{ bgcolor: 'primary.main', borderBottomLeftRadius: 5, borderBottomRightRadius: 5 }}>
+		<Container sx={{
+			bgcolor: 'primary.main',
+			paddingBottom: 2,
+			borderBottomLeftRadius: 5,
+			borderBottomRightRadius: 5
+		}}>
 			<Container sx={{ bgcolor: 'white', borderRadius: 2 }}>
-				<h1>This is the User Page</h1>
-				<p>Logged in = {isLoggedIn().toString()}</p>
-				<Outlet />
+				<Stack>
+					<Grid container>
+						<Grid xs={4}>
+							<Label variant='h5'>Facility Name:</Label>
+						</Grid>
+						<Grid xs={8}>
+							<Content>{userData.facilityName}</Content>
+						</Grid>
+					</Grid>
+					<Grid container>
+						<Grid xs={4}>
+							<Label variant='h5'>Address:</Label>
+						</Grid>
+						<Grid xs={8}>
+							<Content>{`${address.street1} ${address.city}, ${address.state} ${address.zip}`}</Content>
+						</Grid>
+					</Grid>
+					<Grid container>
+						<Grid xs={4}>
+							<Label variant='h5'>Phone:</Label>
+						</Grid>
+						<Grid xs={8}>
+							<Content>{userData.facilityPhone}</Content>
+						</Grid>
+					</Grid>
+					<Grid container>
+						<Grid xs={4}>
+							<Label variant='h5'>Email:</Label>
+						</Grid>
+						<Grid xs={8}>
+							<Content>{userData.facilityEmail}</Content>
+						</Grid>
+					</Grid>
+				</Stack>
 			</Container>
 		</Container>
 	);
