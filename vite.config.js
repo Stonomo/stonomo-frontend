f46da5b8b30ed6b0f346ad5c5530a8d6fa428cc9
Stella Dunn <stellacdunn@gmail.com>
2024-03-18 10:23:49 -0600

    Added chunking logic and port configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { dependencies } from './package.json';
+import react from '@vitejs/plugin-react-swc';
 
+function renderChunks(deps) {
+  let chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (['react', 'react-router', 'react-router-dom', 'react-dom', 'jwt-decode', 'dayjs'].includes(key)) return;
+    if (key.startsWith('@')) return
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-router', 'react-router-dom', 'react-dom', 'jwt-decode'],
+          mui: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled', '@fontsource/roboto'],
+          report: ['dayjs', '@mui/x-date-pickers'],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true
+  },
+  preview: {
+    port: 80,
+    strictPort: true,
+    host: true
+  }
 })
