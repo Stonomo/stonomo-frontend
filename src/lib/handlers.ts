d55a63a2e9dc6c15d2181316e41cb93d4041864c
Stella Dunn <stellacdunn@gmail.com>
2024-04-04 09:04:22 -0600

    Corrected type on maskPhoneInput

--- src/lib/handlers.ts
+++ src/lib/handlers.ts
@@ -1,4 +1,6 @@
-export function maskPhoneInput(e: Event & { target: HTMLInputElement }) {
+import { FormEvent } from "react"
+
+export function maskPhoneInput(e: FormEvent<HTMLDivElement> & { target: HTMLInputElement }) {
 	const m = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/) || ''
 	e.target.value = !m[2] ? m[1] : '(' + m[1] + ')' + m[2] + (m[3] ? '-' + m[3] : '')
 }
