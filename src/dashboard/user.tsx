a35fc1f3077270c7d66d6e403ca0ee51b7d2ece2
Stella Dunn <stellacdunn@gmail.com>
2024-04-06 10:23:48 -0600

    Redesigned user page

--- src/dashboard/user.tsx
+++ src/dashboard/user.tsx
@@ -1,22 +1,23 @@
-import { Container, Unstable_Grid2 as Grid, Stack, Typography, styled } from "@mui/material";
+import { Container, Unstable_Grid2 as Grid, Stack, TextField, Typography, styled } from "@mui/material";
 import { useLoaderData } from "react-router";
+import { profileFields } from "../lib/types";
 
 const Label = styled(Typography)({
-	textAlign: 'right',
-	height: '100%',
-	width: '100%',
-	padding: '10px'
-})
-
-const Content = styled(Typography)({
 	textAlign: 'left',
 	height: '100%',
 	width: '100%',
-	paddingTop: '15px'
+	paddingTop: '10px',
+	fontWeight: 'bold'
+})
+
+const Content = styled(TextField)({
+	textAlign: 'left',
+	height: '100%',
+	width: '100%',
 })
 
 export function UserPage() {
-	const userData = useLoaderData()
+	const userData = useLoaderData() as profileFields
 	const address = userData.facilityAddress
 
 	return (
@@ -26,40 +27,19 @@ export function UserPage() {
 			borderBottomLeftRadius: 5,
 			borderBottomRightRadius: 5
 		}}>
-			<Container sx={{ bgcolor: 'white', borderRadius: 2 }}>
+			<Container sx={{ bgcolor: 'white', paddingBottom: 2, borderRadius: 2 }}>
 				<Stack>
-					<Grid container>
-						<Grid xs={4}>
-							<Label variant='h5'>Facility Name:</Label>
-						</Grid>
-						<Grid xs={8}>
-							<Content>{userData.facilityName}</Content>
-						</Grid>
-					</Grid>
-					<Grid container>
-						<Grid xs={4}>
-							<Label variant='h5'>Address:</Label>
-						</Grid>
-						<Grid xs={8}>
-							<Content>{`${address.street1} ${address.city}, ${address.state} ${address.zip}`}</Content>
-						</Grid>
-					</Grid>
-					<Grid container>
-						<Grid xs={4}>
-							<Label variant='h5'>Phone:</Label>
-						</Grid>
-						<Grid xs={8}>
-							<Content>{userData.facilityPhone}</Content>
-						</Grid>
-					</Grid>
-					<Grid container>
-						<Grid xs={4}>
-							<Label variant='h5'>Email:</Label>
-						</Grid>
-						<Grid xs={8}>
-							<Content>{userData.facilityEmail}</Content>
-						</Grid>
-					</Grid>
+					<Label>Facility Name:</Label>
+					<Content disabled value={userData.facilityName} />
+					<Label>Address:</Label>
+					<Typography paddingLeft='5px'>{address.street1}
+						{address.street2 && (<br />)}{address.street2}
+						{address.street3 && (<br />)}{address.street3}
+						<br />{address.city}, {address.state} {address.zip}</Typography>
+					<Label>Phone:</Label>
+					<Content disabled value={userData.facilityPhone} />
+					<Label>Email:</Label>
+					<Content disabled value={userData.facilityEmail} />
 				</Stack>
 			</Container>
 		</Container>
