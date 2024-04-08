b0ac6eade6b6e05b0277f32902cc8b7fbda582ee
Stella Dunn <stellacdunn@gmail.com>
2024-04-08 13:10:02 -0600

    Made profile page match style

--- src/ui/profile.tsx
+++ src/ui/profile.tsx
@@ -10,14 +10,10 @@ import {
 	styled
 } from "@mui/material";
 import { maskPhoneInput } from "../lib/handlers";
-import { FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { profileFields } from "../lib/types";
+import { Label } from "../lib/styled";
 
-const Label = styled(Typography)({
-	height: '100%',
-	width: '100%',
-	padding: '10px'
-})
 
 export function ProfilePage() {
 	const profile = useLoaderData() as profileFields
@@ -29,7 +25,7 @@ export function ProfilePage() {
 	})
 	const [changed, setChanged] = useState(false)
 
-	function handleChange(e: FormEvent<HTMLInputElement> & { target: HTMLInputElement }) {
+	function handleChange(e: ChangeEvent<HTMLInputElement>) {
 		setFormData(({ ...formData, [e.target.name]: e.target.value }))
 		setChanged(true)
 	}
@@ -44,86 +40,68 @@ export function ProfilePage() {
 			<Container sx={{ bgcolor: 'white', borderRadius: 2, paddingBottom: 2 }}>
 				<Form method='POST'>
 					<Stack>
-						<Grid xs={4}>
-							<Label variant='h5'>Facility Name:</Label>
-						</Grid>
-						<Grid xs={8}>
-							<TextField
-								onChange={handleChange}
-								defaultValue={formData.facilityName}
-								fullWidth
-							/>
-						</Grid>
-						<Grid xs={4}>
-							<Label variant='h5'>Address:</Label>
-						</Grid>
-						<Grid xs={8}>
-							<Stack direction='column'>
-								<TextField
-									id='street1'
-									onChange={handleChange}
-									defaultValue={formData.facilityAddress.street1}
-									placeholder="Street 1"
-									fullWidth
-								/>
-								<TextField
-									id='street2'
-									onChange={handleChange}
-									defaultValue={formData.facilityAddress.street2}
-									placeholder="Street 2"
-									fullWidth
-								/>
-								<TextField
-									id='street3'
-									onChange={handleChange}
-									defaultValue={formData.facilityAddress.street3}
-									placeholder="Street 3"
-									fullWidth
-								/>
-								<TextField
-									id='city'
-									onChange={handleChange}
-									defaultValue={formData.facilityAddress.city}
-									placeholder="City"
-									fullWidth
-								/>
-								<TextField
-									id='state'
-									onChange={handleChange}
-									defaultValue={formData.facilityAddress.state}
-									placeholder="State"
-									fullWidth
-								/>
-								<TextField
-									id='zip'
-									onChange={handleChange}
-									defaultValue={formData.facilityAddress.zip}
-									placeholder="Zip"
-									fullWidth
-								/>
-							</Stack>
-						</Grid>
-						<Grid xs={4}>
-							<Label variant='h5'>Phone:</Label>
-						</Grid>
-						<Grid xs={8}>
-							<TextField
-								onChange={handleChange}
-								onInput={maskPhoneInput}
-								defaultValue={formData.facilityPhone}
-								fullWidth
-							/>
-						</Grid>
-						<Grid xs={4}>
-							<Label variant='h5'>Email:</Label>
-						</Grid>
-						<Grid xs={8}>
-							<TextField
-								onChange={handleChange}
-								defaultValue={formData.facilityEmail}
-								fullWidth
-							/>
-						</Grid>
+						<Label>Facility Name:</Label>
+						<TextField
+							onChange={handleChange}
+							defaultValue={formData.facilityName}
+							fullWidth
+						/>
+						<Label>Address:</Label>
+						<TextField
+							id='street1'
+							onChange={handleChange}
+							defaultValue={formData.facilityAddress.street1}
+							placeholder="Street 1"
+							fullWidth
+						/>
+						<TextField
+							id='street2'
+							onChange={handleChange}
+							defaultValue={formData.facilityAddress.street2}
+							placeholder="Street 2"
+							fullWidth
+						/>
+						<TextField
+							id='street3'
+							onChange={handleChange}
+							defaultValue={formData.facilityAddress.street3}
+							placeholder="Street 3"
+							fullWidth
+						/>
+						<TextField
+							id='city'
+							onChange={handleChange}
+							defaultValue={formData.facilityAddress.city}
+							placeholder="City"
+							fullWidth
+						/>
+						<TextField
+							id='state'
+							onChange={handleChange}
+							defaultValue={formData.facilityAddress.state}
+							placeholder="State"
+							fullWidth
+						/>
+						<TextField
+							id='zip'
+							onChange={handleChange}
+							defaultValue={formData.facilityAddress.zip}
+							placeholder="Zip"
+							fullWidth
+						/>
+						<Label>Phone:</Label>
+						<TextField
+							onChange={handleChange}
+							onInput={maskPhoneInput}
+							defaultValue={formData.facilityPhone}
+							fullWidth
+						/>
+						<Label>Email:</Label>
+						<TextField
+							onChange={handleChange}
+							defaultValue={formData.facilityEmail}
+							fullWidth
+						/>
 						{changed && <Container maxWidth='md'
 							sx={{ textAlign: 'center', margin: 1 }}>
 							<Button
