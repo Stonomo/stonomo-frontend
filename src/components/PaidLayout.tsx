90e4d3ea29b6d82408ca45dda8c44b331f23c7e1
Stella Dunn <stellacdunn@gmail.com>
2024-05-10 07:54:16 -0600

    Fixed /subscribe/subscribe redirect error

--- src/components/PaidLayout.tsx
+++ src/components/PaidLayout.tsx
@@ -1,16 +1,14 @@
 import { Outlet, useNavigate } from "react-router-dom"
 import { useAuth } from "../hooks/useAuth"
-import { Box, Button, Container } from "@mui/material"
 import { useEffect } from "react"
-import { NavBar } from "./NavBar"
 
 export function PaidLayout() {
-	const { isPaidUser, logout } = useAuth()
+	const { isPaidUser } = useAuth()
 	const navigate = useNavigate()
 
 	useEffect(() => {
 		if (!isPaidUser()) {
-			return navigate('subscribe')
+			return navigate('/dashboard/subscribe')
 		}
 	})
 
